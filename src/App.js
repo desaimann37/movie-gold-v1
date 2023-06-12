89e813d1525ffc52534deaf5ad6f9ec234043721
MANN DESAI <desaimann37@gmail.com>
2023-06-12 16:33:39 +0530

    adding ...

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css'
 import Appbar from './componets/Appbar'
 import Student from './componets/student';
 function App(){
-
   return (
-
       <div className="App">
         <Appbar/>
         <Student/>
       </div>
   );
-
 }
 export default App;
