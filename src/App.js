2c661eb5fbaed56fe94502fbb6e9452c6c6aab2e
MANN DESAI <desaimann37@gmail.com>
2023-06-16 11:10:11 +0530

    Adding Component Which can fetch all data from backend server with asynchronous Fetch API Request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css'
 import Appbar from './componets/Appbar'
 import Student from './componets/student';
+import AxiosConfiguration from './appi/AxiosConfiguration';
+import { Component, useState } from 'react';
+
+
 function App(){
+  const[showComponent , setshowComponent] = useState(false);
+  const handleClick = ()=>{
+    setshowComponent(true);
+  }
+
   return (
       <div className="App">
         <Appbar/>
         <Student/>
+        <button onClick={handleClick}>Get All Data From BackEnd</button>
+
+        {showComponent && <ComponentNew/>}
       </div>
   );
 }
+
+const ComponentNew = ()=>{
+  return <h3>
+    <AxiosConfiguration />
+  </h3>
+}
+
 export default App;
--- src/appi/axiosConfiguration.js
+++ src/appi/axiosConfiguration.js
@@ -1,8 +1,53 @@
 import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import '../CSS/FetchedDataStyle.css'
+import {Link} from 'react-router-dom'
 
-export default axios.create({
+function AxiosConfiguration() {
+    const[data , setData] = useState([]);
+    useEffect(()=>{
+        const fetchData = async() => {
+        try{
+            const response = await axios.get('http://localhost:9092/student/getAll');
+            setData(response.data);
+        }
+        catch(error){
+            console.log('Error fetching data : ' , error);
+        }
+    };
+    
+    fetchData();
+    } , []);
 
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
-    headers: {"ngrok-skip-browser-warning": "true"}
 
-});
+
+  return (
+    <div >
+        <ul>
+            {data.map((item) => (
+                <table>
+                    <div key={item.id}>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.address}</td>
+                        </tr>
+                    </tbody>
+                    </div>
+                </table>
+            ))}
+        </ul>
+    </div>
+    
+  )
+}
+
+export default AxiosConfiguration
